4cb648bc8a50a47cef754c82ecead36833ec3713
Santyru <santiago0988@hotmail.com>
2023-03-10 20:47:53 -0300

    map basic input

--- project/scenes/actors/Player.gd
+++ project/scenes/actors/Player.gd
@@ -1,13 +1,24 @@
 extends KinematicBody2D
 
 export(int) var speed = 400
+# import bullet scene
 
+var lifes = 3
+var direction = Vector2()
+var velocity = Vector2()
+
+func shoot():
+	pass
+	
 func get_input ():
 	direction.x = 0
 	if Input.is_action_pressed("ui_right"):
 		direction.x = 1
 	if Input.is_action_pressed("ui_left"):
 		direction.x = -1
+	if Input.is_action_pressed("ui_accept"):
+		shoot() # and not bullet instance 
+		
 func _ready():
 	pass
 
