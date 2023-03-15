1dff88508b4eb38282263ee32576fb8f74a6e82c
Santyru <santiago0988@hotmail.com>
2023-03-15 19:37:25 -0300

    add child node

--- project/scenes/actors/Proyectile.gd
+++ project/scenes/actors/Proyectile.gd
@@ -5,12 +5,10 @@ var direction = Vector2(0, -1)
 
 func _physics_process(delta):
 	var velocity = direction * bullet_speed
-	var collision = move_and_collide(velocity * delta)
+	var _collision = move_and_collide(velocity * delta)
 	
 func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_VisibilityNotifier2D_screen_exited():
+	self.queue_free()
