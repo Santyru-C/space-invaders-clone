74d749c3bad511ba56e8b41931ff476ca9524372
Santyru <santiago0988@hotmail.com>
2023-03-17 17:34:41 -0300

    move to actors folder

--- project/scenes/Proyectile.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-export(int) var proyectile_speed = 0
-
-func _process(delta):
-	position += transform.y * proyectile_speed * delta
-
-func _on_VisibilityNotifier2D_screen_exited():
-	self.queue_free()
