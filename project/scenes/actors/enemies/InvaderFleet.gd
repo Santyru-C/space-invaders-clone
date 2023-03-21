ae0b7164afbed68e3a995d81630ab2efef7e567a
Santyru <santiago0988@hotmail.com>
2023-03-21 20:06:35 -0300

    connect child signals

--- project/scenes/actors/enemies/InvaderFleet.gd
+++ project/scenes/actors/enemies/InvaderFleet.gd
@@ -2,12 +2,14 @@ extends Area2D
 var test_enemy = preload("res://scenes/actors/enemies/Enemy1.tscn")
 var direction = 1
 
+func conect_ship_signal():
+	pass
 func get_colored_brick(color):
 	var formated_string = "res://scenes/%s_brick.tscn" % color
 	var colored_brick_scene = load(formated_string)
 	var colored_brick = colored_brick_scene.instance()
 	return colored_brick
-	
+
 func place_invader(invader, position_x, position_y):
 	invader.set_position(Vector2(position_x, position_y))
 	call_deferred("add_child", invader)
@@ -39,7 +41,8 @@ func _on_Timer_timeout():
 
 
 func _on_InvaderFleet_area_entered(area):
-	if "Trigger" in area.get_name():
-		direction *= -1
-		position += transform.y * 4
+	pass
+	#if "Trigger" in area.get_name():
+	#	direction *= -1
+	#	position += transform.y * 4
 	
