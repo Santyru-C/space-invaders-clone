df5782c551c874a739bd7339302d851c757f3f34
Santyru <santiago0988@hotmail.com>
2023-03-21 19:58:07 -0300

    add instancing function

--- project/scenes/actors/enemies/FleetColumn.gd
+++ project/scenes/actors/enemies/FleetColumn.gd
@@ -1,16 +1,25 @@
 extends Area2D
 
+var ships = 5
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+func get_enemy_type(type):
+	var formated_string = "res://scenes/actors/enemies/Enemy%s.tscn" % type
+	var enemy_scene = load(formated_string)
+	var enemy = enemy_scene.instance()
+	return enemy
 
-
-# Called when the node enters the scene tree for the first time.
+func place_invader(invader, position_y):
+	invader.set_position(Vector2(0, position_y))
+	call_deferred("add_child", invader)
+	
+func generate_invader_column():
+	var column_pattern = ["1", "2", "2", "3", "3"]
+	var position_y = 0
+	# var current_color = ""
+	for row in 5:
+		var current_enemy = get_enemy_type(column_pattern[row])
+		place_invader(current_enemy, position_y)
+		position_y += 56
+	
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	generate_invader_column()
