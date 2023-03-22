fd42c1f629921e202202f3176f336e901a6a64ef
Santyru <santiago0988@hotmail.com>
2023-03-22 19:04:42 -0300

    add function

--- project/scenes/actors/enemies/InvaderFleet.gd
+++ project/scenes/actors/enemies/InvaderFleet.gd
@@ -1,38 +1,46 @@
 extends Area2D
 const fleet_column_scene = preload("res://scenes/actors/enemies/FleetColumn.tscn")
+var fleet_columns = 11
 var direction = 1
 
-func get_colored_brick(color):
-	var formated_string = "res://scenes/%s_brick.tscn" % color
-	var colored_brick_scene = load(formated_string)
-	var colored_brick = colored_brick_scene.instance()
-	return colored_brick
+func _on_column_area_entered(area):
+	if "Trigger" in area.get_name():
+		print("entered: ", area)
+		direction *= -1
+		position += transform.y * 5
 
+func _on_column_down():
+	fleet_columns -= 1
+	if fleet_columns == 0:
+		self.queue_free()
+	
 func place_invader(invader, position_x):
 	invader.set_position(Vector2(position_x, 48))
-	call_deferred("add_child", invader)
+	self.add_child(invader)
+	#call_deferred("add_child", invader)
 	
 func generate_enemy_fleet():
-		var position_x = -396
+		var position_x = -362
 		for column in 11:
-			if column != 1:
+			if column != 0:
 				position_x += 72
 			var fleet_column = fleet_column_scene.instance()
+			fleet_column.connect("area_entered", self, "_on_column_area_entered")
+			fleet_column.connect("column_down", self, "_on_column_down")
 			place_invader(fleet_column, position_x)
+			
 func _ready():
 	generate_enemy_fleet()
-
-func _process(delta):
-	pass
-
+	print(self.get_children())
 
 func _on_Timer_timeout():
 	position += transform.x * -10 * direction
 
 
-func _on_InvaderFleet_area_entered(area):
+func _on_InvaderFleet_area_entered(_area):
 	pass
 	#if "Trigger" in area.get_name():
 	#	direction *= -1
 	#	position += transform.y * 4
 	
+
