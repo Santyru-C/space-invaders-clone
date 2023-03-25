f5c4a4eb5f753420e381f6792c3a4716fa091414
Santyru <santiago0988@hotmail.com>
2023-03-25 20:29:40 -0300

    cleanup

--- project/scenes/actors/ShipTemplate.gd
+++ project/scenes/actors/ShipTemplate.gd
@@ -6,5 +6,4 @@ export(int) var speed = 400
 func shoot():
 	var proyectile = proyectile_scene.instance()
 	get_tree().get_root().add_child(proyectile)
-	print(get_tree().get_root().get_name())
 	proyectile.position = $Position2D.global_position
