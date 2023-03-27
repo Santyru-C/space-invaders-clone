46ed9d3f589daf61887545d4be69a80ebc2df54e
Santyru <santiago0988@hotmail.com>
2023-03-27 20:53:06 -0300

    add new game_over_screen function

--- project/scenes/menu/HUD.gd
+++ project/scenes/menu/HUD.gd
@@ -1,6 +1,28 @@
 extends CanvasLayer
+signal start_game
 
-func _ready():
-	$LifesLabel.hide()
-	$ScoreLabel.hide()
+func update_ScoreLabel(value):
+	$ScoreLabel.text = "Score: %s" % value
+
+func update_LifeLabel(value):
+	$LifeLabel.text = "Life: %s" % value
 	
+func _ready():
+	$LifeLabel.hide()
+	$ScoreLabel.hide()
+
+func toggle_display():
+	var HUD_children = self.get_children()
+	for child in HUD_children:
+		if child.is_visible_in_tree():  #There's no ternary operator in GDScript
+			child.hide()
+		else:
+			child.show()
+			
+func call_game_over_screen():
+	$TitleLabel.text = "Game Over"
+	toggle_display()
+
+func _on_StartButton_pressed():
+	emit_signal("start_game")
+	toggle_display()
