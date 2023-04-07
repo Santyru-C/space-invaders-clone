4de5654d6657204815bf07f3d3d552fef8f5ea1b
Santyru <santiago0988@hotmail.com>
2023-04-07 18:40:18 -0300

    update sprites

--- project/scenes/actors/enemies/EnemyTemplate.gd
+++ project/scenes/actors/enemies/EnemyTemplate.gd
@@ -3,10 +3,15 @@ export(int) var shot_interval = 1000
 export(int) var score_amount = 0
 signal ship_down
 signal add_score(value)
+var current_position = Vector2()
+var current_frame = 0
+onready var _animated_sprite = $AnimatedSprite
+export(int) var animation_frames = 0
 
 func _process(_delta):
 	if randi() % shot_interval == 1:
 		shoot()
+		
 
 func _on_EnemyTemplate_area_entered(area):
 	if "Bullet" in area.get_name():
